d54748f54d76f01b02bb3a7fa9b4f3b7971bfdcb
Gal Levin <glevin2@gmail.com>
2019-05-26 20:29:18 +0300

    Added update script

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ echo "Switching to Monitor mode..."
 sudo airmon-ng start wlan0
 
 echo "60 Second scan..."
-sudo tshark -i wlan0mon -a duration:$SCAN_TIME -w last -F pcap
+sudo tshark -i wlan0mon -a duration:$SCAN_TIME -w last.pcap -F pcap
 
 echo "Anlayzing and sending to server"
 python ./prober-service.py
---
+++ update.sh
@@ -0,0 +1,6 @@
+git fetch origin master
+git reset --hard FETCH_HEAD
+git clean -df
+
+chmod +x run.sh
+chmod +x update.sh
